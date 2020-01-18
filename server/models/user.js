4edde63e0d9b66e8f129ea29f580f849a36372e8
Tareq El Dandachi <tareqdandachi@gmail.com>
2020-01-17 19:38:57 -0500

    This isn't an init commit. Trust me. Don't try to prove me wrong

---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  high_score: String,
+  googleid: String,
+});
+
+// compile model from schema
+module.exports = mongoose.model("user", UserSchema);
